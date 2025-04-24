e6de91c129d6f2f7e02d9d2c80112440f6539862
Assia Ouaoua <asoua1606@gmail.com>
2025-04-24 15:41:40 +0100

    Task 1

--- Horse.java
+++ Horse.java
@@ -8,16 +8,22 @@
 public class Horse
 {
     //Fields of class Horse
+    char horseSymbol;
+    String horseName;
+    double horseConfidence;
+    int distanceTravelled;
+    boolean hasFallen;
     
-    
-      
     //Constructor of class Horse
     /**
      * Constructor for objects of class Horse
      */
     public Horse(char horseSymbol, String horseName, double horseConfidence)
     {
-       
+       this.horseSymbol = horseSymbol;
+       this.horseName = horseName;
+       this.horseConfidence = horseConfidence;
+       this.distanceTravelled = 0;
     }
     
     
@@ -25,52 +31,52 @@ public class Horse
     //Other methods of class Horse
     public void fall()
     {
-        
+        hasFallen = true;
     }
     
     public double getConfidence()
     {
-        
+        return horseConfidence;
     }
     
     public int getDistanceTravelled()
     {
-        
+        return distanceTravelled;
     }
     
     public String getName()
     {
-        
+        return horseName;
     }
     
     public char getSymbol()
     {
-        
+        return horseSymbol;
     }
     
     public void goBackToStart()
     {
-        
+        distanceTravelled = 0;
     }
     
     public boolean hasFallen()
     {
-        
+        return hasFallen;
     }
 
     public void moveForward()
     {
-        
+        distanceTravelled += 1;
     }
 
     public void setConfidence(double newConfidence)
     {
-        
+        horseConfidence = newConfidence;
     }
     
     public void setSymbol(char newSymbol)
     {
-        
+        horseSymbol = newSymbol;
     }
     
 }
